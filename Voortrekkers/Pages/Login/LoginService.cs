cd383bdfc3f65107e6e2f5a65d650a3bb353e0af
Johann Nefdt <58251579+nefdtie101@users.noreply.github.com>
2022-09-17 16:56:01 +0200

    first commit

---
+++ Voortrekkers/Pages/Login/LoginService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Voortrekkers.Pages.Login;
+
+public class LoginService
+{
+    private readonly ILocalStorageService _localStorageService;
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+    private readonly HttpClient _httpClient;
+    
+    
+    public LoginService(ILocalStorageService localStorageService , AuthenticationStateProvider authenticationStateProvider , HttpClient httpClient)
+    {
+        _localStorageService = localStorageService;
+        _authenticationStateProvider = authenticationStateProvider;
+        _httpClient = httpClient;
+    }
+
+  public async Task Login(LoginModel login)
+    {
+        var result = await _httpClient.PostAsJsonAsync("http://localhost:5111/login", login);
+        var token = await result.Content.ReadAsStringAsync();
+        await _localStorageService.SetItemAsync("token", token);
+        await _authenticationStateProvider.GetAuthenticationStateAsync();
+    }
+}
---
+++ Voortrekkers/Program.cs
@@ -0,0 +1,19 @@
+global using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Voortrekkers;
+using Voortrekkers.Pages.Login;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<LoginService>();
+
+await builder.Build().RunAsync();
