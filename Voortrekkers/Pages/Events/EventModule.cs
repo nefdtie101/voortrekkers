f8d66729b7aed7fa29b0f1f87c499dd7cde5567e
Johann Nefdt <johann@nefdtco.co.za>
2023-01-16 19:00:56 +0200

    added email message

--- Voortrekkers/Pages/Events/EventModule.cs
+++ Voortrekkers/Pages/Events/EventModule.cs
@@ -1,19 +1,13 @@
 ﻿using DataBaseModles;
 public class EventModel
 {
-    
-    
     public string IdEvent { get; set; }
-    
     public EventType eventType { get; set; }
-    
     public string EventName { get; set; }
-    
     public string EventDiscription { get; set; }
-    
     public DateTime EventDate { get; set; }
-    
     public string idOrganization { get; set; }
-    
     public string RedirectUri { get; set; }
+    
+    public string Message { get; set; }
 }
